f529443b85518881276386ed37d804f0b87048ad
rendyprayoga <rendyprayoga806@gmail.com>
2024-09-29 15:00:40 +0700

    add file

---
+++ src/components/SectionTwo.vue
@@ -0,0 +1,36 @@
+<div class="section-we">
+      <h1 class="we-are">we are</h1>
+      <p class="we-description">
+        team of like-minded people, socially active companies, media
+        personalities, volunteers who help low-income shelters and foster carers
+        to support homeless animals.
+      </p>
+      <h1 class="we-are">Our primary goal</h1>
+      <p class="we-description">
+        is to provide animals with the most necessary things - food, medicine,
+        and urgent needs for the livelihood of shelters. We believe in our power
+        and the power of conscious citizens who care about the environment and
+        their health, who understand the importance of helping each other,
+        especially our little friends. Growth begins with your care for your
+        loved ones. Animals are one of the closest creatures to humans who
+        remain with us throughout our lives. We all know the healing power of
+        warmth, grace, and big loving eyes.
+      </p>
+      <blockquote class="big-text-work quote">
+        "Animals need to have friends just like humans." <br />
+        - James Herriot, All Creatures Great and Small
+      </blockquote>
+      <p class="we-description">
+        Who else if not we should support our younger brothers, especially in
+        difficult times? Let's be friends!
+      </p>
+      <div class="youtube-container">
+        <iframe
+          src="https://www.youtube.com/embed/5Wk1rp99B7o"
+          title="YouTube video player"
+          frameborder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          allowfullscreen
+        ></iframe>
+      </div>
+    </div>
