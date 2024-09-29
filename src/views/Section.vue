d1ea936d0e9980759c961d980f7b61e3b1d28b7f
rendyprayoga <rendyprayoga806@gmail.com>
2024-09-29 19:19:44 +0700

    add

---
+++ src/views/Section.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <SectionOne />
+    <SectionTwo />
+  </div>
+</template>
+<script>
+import SectionOne from "@/components/SectionOne.vue";
+import SectionTwo from "@/components/SectionTwo.vue";
+</script>
