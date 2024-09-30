7f7a4c453ba69b2ee78508d1665f397b9646685c
rendyprayoga <rendyprayoga806@gmail.com>
2024-09-30 12:50:10 +0700

    update and update package-lock.json

--- src/components/SectionTwo.vue
+++ src/components/SectionTwo.vue
@@ -1,36 +1,79 @@
-<div class="section-we">
-      <h1 class="we-are">we are</h1>
-      <p class="we-description">
-        team of like-minded people, socially active companies, media
-        personalities, volunteers who help low-income shelters and foster carers
-        to support homeless animals.
-      </p>
-      <h1 class="we-are">Our primary goal</h1>
-      <p class="we-description">
-        is to provide animals with the most necessary things - food, medicine,
-        and urgent needs for the livelihood of shelters. We believe in our power
-        and the power of conscious citizens who care about the environment and
-        their health, who understand the importance of helping each other,
-        especially our little friends. Growth begins with your care for your
-        loved ones. Animals are one of the closest creatures to humans who
-        remain with us throughout our lives. We all know the healing power of
-        warmth, grace, and big loving eyes.
-      </p>
-      <blockquote class="big-text-work quote">
-        "Animals need to have friends just like humans." <br />
-        - James Herriot, All Creatures Great and Small
-      </blockquote>
-      <p class="we-description">
-        Who else if not we should support our younger brothers, especially in
-        difficult times? Let's be friends!
-      </p>
-      <div class="youtube-container">
-        <iframe
-          src="https://www.youtube.com/embed/5Wk1rp99B7o"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
-      </div>
+<template>
+  <div class="section-we">
+    <h1 class="we-are">we are</h1>
+    <p class="we-description">
+      team of like-minded people, socially active companies, media
+      personalities, volunteers who help low-income shelters and foster carers
+      to support homeless animals.
+    </p>
+    <h1 class="we-are">Our primary goal</h1>
+    <p class="we-description">
+      is to provide animals with the most necessary things - food, medicine, and
+      urgent needs for the livelihood of shelters. We believe in our power and
+      the power of conscious citizens who care about the environment and their
+      health, who understand the importance of helping each other, especially
+      our little friends. Growth begins with your care for your loved ones.
+      Animals are one of the closest creatures to humans who remain with us
+      throughout our lives. We all know the healing power of warmth, grace, and
+      big loving eyes.
+    </p>
+    <blockquote class="big-text-work quote">
+      "Animals need to have friends just like humans." <br />
+      - James Herriot, All Creatures Great and Small
+    </blockquote>
+    <p class="we-description">
+      Who else if not we should support our younger brothers, especially in
+      difficult times? Let's be friends!
+    </p>
+    <div class="youtube-container">
+      <iframe
+        src="https://www.youtube.com/embed/5Wk1rp99B7o"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen
+      ></iframe>
     </div>
+  </div>
+</template>
+<style>
+.section-we {
+  padding-top: 100px;
+  font-family: "DM sans", sans-serif;
+  padding-left: 182px;
+  padding-right: 200px;
+}
+.we-description {
+  color: #000;
+  text-align: left;
+  margin-top: 0.5em;
+  margin-bottom: 1em;
+  font-size: 2.5vw;
+  line-height: 1.5em;
+  display: block;
+}
+.we-are {
+  color: #000;
+  margin-top: 0;
+  margin-bottom: 0;
+  font-size: 4vw;
+  font-weight: 700;
+  line-height: 1.25em;
+}
+.youtube-container {
+  position: relative;
+  padding-bottom: 58.25%;
+  height: 0;
+  overflow: hidden;
+  max-width: 100%;
+  background-color: #000;
+}
+
+.youtube-container iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
--- src/views/WeAre.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <OurProject />
-</template>
-<script setup>
-import OurProject from "@/components/OurProject.vue";
-</script>
