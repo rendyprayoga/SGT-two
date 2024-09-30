15f26682d56e5b4c9fef2064a8a913042835c4c3
rendyprayoga <93699796+rendyprayoga@users.noreply.github.com>
2024-09-30 07:51:56 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,76 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <RouterView />
+  <!-- <div>
+    <OurProject />
+  </div> -->
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+import OurProject from "./components/OurProject.vue";
+import { RouterView } from "vue-router";
+export default {
+  name: "App",
+  components: {
+    OurProject,
+  },
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  padding: 0;
+  height: 100vh;
+  font-family: "DM sans", sans-serif;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.donate-container {
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  z-index: 10;
+}
+.brutalist-button {
+  --bg: #fff;
+  --text-color: black;
+  --hover-text-color: black;
+
+  color: var(--text-color);
+  cursor: pointer;
+  padding: 0.5em 1em;
+  text-decoration: none;
+  border: 3px solid var(--text-color);
+  position: relative;
+  font-size: 2vw;
+  background-color: #fff;
+  line-height: 1.5em;
+  display: inline-block;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+.brutalist-button:hover {
+  color: var(--hover-text-color);
+  transform: translate(0.25rem, -0.25rem);
+  background-color: rgba(red, green, blue, 0);
+  box-shadow: -0.25rem 0.25rem var(--bg);
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.brutalist-button:active {
+  transform: translate(10);
+  box-shadow: none;
+}
+.big-text-work.quote {
+  border-left-width: 0.25rem;
+  border-left-color: #ff97d0;
+  border-left-style: solid;
+  margin-top: 0;
+  margin-bottom: 0;
+  margin-left: 4rem;
+  padding: 1rem;
+  font-style: italic;
+  font-weight: 400;
+  font-size: 2vw;
 }
 </style>
+<!-- .button::after {
+     
+    } -->
